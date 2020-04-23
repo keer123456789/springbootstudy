511075f8ed7aceb1c37890cb03b2b3885404aee9
keer123456789 <15110074528@163.com>
2020-04-23 15:59:26 +0800

    feat: 🎸 add new moudle :sercurity_demo_3

---
+++ sercurity_demo_3/src/main/java/com/keer/sercurity_demo_3/SercurityDemo3Application.java
@@ -0,0 +1,13 @@
+package com.keer.sercurity_demo_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SercurityDemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SercurityDemo3Application.class, args);
+    }
+
+}
---
+++ sercurity_demo_3/src/test/java/com/keer/sercurity_demo_3/SercurityDemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keer.sercurity_demo_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SercurityDemo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
